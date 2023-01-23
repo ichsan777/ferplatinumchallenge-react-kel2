d6b33027c18c260fe661d70ef6163b249776db2e
Verika16 <Kebry.poetra@gmail.com>
2023-01-23 23:23:26 +0700

    Perapihan Kebry

--- ferplatinumchallenge-react-kel2/src/config/Router.js
+++ ferplatinumchallenge-react-kel2/src/config/Router.js
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import RenderLayout from "./RenderLayout";
-import SectionAbout from "../components/SectionAbout";
-import SectionWhyUs from "../components/SectionWhyUs";
+import SectionAbout from "../Sections/SectionAbout";
+import SectionWhyUs from "../Sections/SectionWhyUs";
 import SectionTestimonial from "../components/SectionTestimonial";
-import SectionHeroBanner from "../components/SectionHeroBanner";
+import SectionHeroBanner from "../Sections/SectionHeroBanner";
 import SectionCallToAction from "../components/SectionCallToAction";
 import SectionFrequentlyAskedQuestion from "../components/SectionFrequentlyAskedQuestion";
 import SectionHeroBannerCariMobil from "../components/SectionHeroBannerCariMobil";
@@ -12,11 +12,12 @@ import SectionPageTitle from "../Sections/SectionPageTitle";
 import SectionDetailMobil from "../components/SectionDetailMobil";
 import SectionFormDetailMobil from "../components/SectionFormDetailMobil";
 import PageHasilCari from "../pages/PageHasilCari";
+import PageHome from "../pages/PageHome";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <RenderLayout children={[<SectionHeroBanner />, <SectionAbout />, <SectionWhyUs />, <SectionTestimonial />, <SectionCallToAction />, <SectionFrequentlyAskedQuestion />]} />,
+    element: <RenderLayout children={[<PageHome />]} />,
   },
   {
     path: "/carimobil",
