7ce0eddaf510b454e61054c4681cf001a1006800
GerardHarahap <gerard.harahap@gmail.com>
2023-02-06 12:00:21 +0700

    Restructure PageCariMobil

--- src/config/Router.js
+++ src/config/Router.js
@@ -1,12 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import RenderLayout from "./RenderLayout";
-import SectionHeroBannerCariMobil from "../components/SectionHeroBannerCariMobil";
-import SectionFormCariMobil from "../components/SectionFormCariMobil";
-import SectionPageTitle from "../Sections/SectionPageTitle";
-import SectionDetailMobil from "../components/SectionDetailMobil";
-import SectionFormDetailMobil from "../components/SectionFormDetailMobil";
 import PageHasilCari from "../pages/PageHasilCari";
 import PageHome from "../pages/PageHome";
+import PageCariMobil from "../pages/PageCariMobil";
 import PageDetailMobil from "../pages/PageDetailMobil";
 
 export const router = createBrowserRouter([
@@ -16,7 +12,7 @@ export const router = createBrowserRouter([
   },
   {
     path: "/carimobil",
-    element: <RenderLayout children={[<SectionHeroBannerCariMobil />, <SectionFormCariMobil />]} />,
+    element: <RenderLayout children={[<PageCariMobil />]} />,
   },
   {
     path: "/hasilcari",
---
+++ src/pages/PageCariMobil.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SectionHeroBanner from "../Sections/SectionHeroBannerCariMobil";
+import SectionFormCariMobil from "../components/SectionFormCariMobil";
+
+const PageCariMobil = () => {
+  return (
+    <div>
+      <SectionHeroBanner />
+      <SectionFormCariMobil />
+    </div>
+  );
+};
+
+export default PageCariMobil;
